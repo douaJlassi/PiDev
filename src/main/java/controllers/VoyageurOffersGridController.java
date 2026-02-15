b826fee44eb8203acc3cfe69e6de425a4d83ad3e
Dhia7Mestiri <116468664+Dhia7Mestiri@users.noreply.github.com>
2026-02-15 21:55:55 +0100

    Client browse

---
+++ src/main/java/controllers/VoyageurOffersGridController.java
@@ -0,0 +1,83 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.TilePane;
+import entities.Offre;
+import javafx.stage.Stage;
+import repositories.IOffreRepository;
+import repositories.OffreRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+public class VoyageurOffersGridController {
+
+    @FXML private TilePane tilePane;
+    private final IOffreRepository repo = new OffreRepository();
+
+    @FXML
+    public void initialize() { refresh(); }
+
+    @FXML
+    private void onRefresh() { refresh(); }
+
+    public void refresh() {
+        tilePane.getChildren().clear();
+
+        List<Offre> offers = repo.findAllAdmin(); // all offers
+        for (Offre offer : offers) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/OfferCard.fxml"));
+                Parent card = loader.load();
+
+                OfferCardController ctrl = loader.getController();
+                ctrl.setData(offer, this::refresh);
+
+                tilePane.getChildren().add(card);
+            } catch (IOException e) {
+                showError("UI error", e.getMessage());
+            }
+        }
+    }
+
+    private void showError(String title, String msg) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void onMyCart() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CartView.fxml"));
+            Parent root = loader.load();
+
+            CartViewController ctrl = loader.getController();
+            ctrl.loadCart(); // will use Session user
+
+            Stage stage = new Stage();
+            stage.setTitle("My Cart");
+            Scene scene = new Scene(root, 950, 650);
+            scene.getStylesheets().add(getClass().getResource("/css/app.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+
+
+        } catch (Exception e) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setTitle("UI error");
+            a.setHeaderText(null);
+            a.setContentText(e.getMessage());
+            a.showAndWait();
+        }
+    }
+
+
+
+}
