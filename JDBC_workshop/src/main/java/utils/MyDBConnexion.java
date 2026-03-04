4afc7e0b4b59c3bfec9a2aa0ab4431b18867bef7
Dhia7Mestiri <116468664+Dhia7Mestiri@users.noreply.github.com>
2026-03-04 04:48:34 +0100

    Update MyDBConnexion.java

--- JDBC_workshop/src/main/java/utils/MyDBConnexion.java
+++ JDBC_workshop/src/main/java/utils/MyDBConnexion.java
@@ -8,7 +8,7 @@ public class MyDBConnexion {
 
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
-    private static final String URL = "jdbc:mysql://localhost:3306/pidev" ;
+    private static final String URL = "jdbc:mysql://localhost:3306/pi" ;
 
     private Connection cnx;
 
